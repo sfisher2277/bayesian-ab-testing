9a26dfeacdcccde06c1f5a2cb4fb6b27a1f59d8e
sfisher2277 <126615737+sfisher2277@users.noreply.github.com>
2026-07-31 07:38:30 -0400

    spelling correction

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from scipy import stats
 import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="A/B Test Calculator", layout="centered")
-st.title("Bayesian vs. Frequntist A/B Test Calculator")
+st.title("Bayesian vs. Frequentist A/B Test Calculator")
