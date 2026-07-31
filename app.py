2afd8e1d5bc7e4f8942cfde23cf0afbfa431c1ba
sfisher2277 <126615737+sfisher2277@users.noreply.github.com>
2026-07-31 07:35:04 -0400

    added app code

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title="A/B Test Calculator", layout="centered")
+st.title("Bayesian vs. Frequntist A/B Test Calculator")
